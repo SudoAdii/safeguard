fa49196f0d0787fdf475b4ad887ca774fa9659cd
Sudo Adi <141738671+SudoAdii@users.noreply.github.com>
2025-05-27 22:36:53 +0530

    Update react-telegram-login.d.ts

--- Mini_app/TG/src/@types/react-telegram-login.d.ts
+++ Mini_app/TG/src/@types/react-telegram-login.d.ts
@@ -1,15 +1,25 @@
-declare module 'react-telegram-login' {  
-    import * as React from 'react';  
-  
-    interface TelegramLoginButtonProps {  
-      dataOnauth: (response: any) => void; // You may further specify the response type as needed  
-      botName: string;  
-      buttonSize?: number;  
-      cornerRadius?: number;  
-      theme?: 'light' | 'dark';  
-    }  
-  
-    const TelegramLoginButton: React.FC<TelegramLoginButtonProps>;  
-  
-    export default TelegramLoginButton;  
-  }  
+declare module 'react-telegram-login' {
+  import * as React from 'react';
+
+  interface TelegramUser {
+    id: number;
+    first_name: string;
+    last_name?: string;
+    username?: string;
+    photo_url?: string;
+    auth_date: number;
+    hash: string;
+  }
+
+  interface TelegramLoginButtonProps {
+    dataOnauth: (response: TelegramUser) => void;
+    botName: string;
+    buttonSize?: number;
+    cornerRadius?: number;
+    theme?: 'light' | 'dark';
+  }
+
+  const TelegramLoginButton: React.FC<TelegramLoginButtonProps>;
+
+  export default TelegramLoginButton;
+}
